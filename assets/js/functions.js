015e59bb1aba54063296785ce3529b0a445f881a
PKyes44 <rugate23@gmail.com>
2024-04-02 11:26:20 +0900

    20240402-01

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -278,3 +278,29 @@ $( document ).ready(function() {
   transitionLabels();
 
 });
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.work-slide');
+const slideCount = slides.length;
+ 
+function showSlide(n) {
+    slides.forEach(slide => slide.style.display = 'none');
+    slides[n].style.display = 'block';
+}
+ 
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slideCount;
+    showSlide(currentSlide);
+}
+ 
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slideCount) % slideCount;
+    showSlide(currentSlide);
+}
+ 
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+    setInterval(nextSlide, 5000); // 3초마다 자동 슬라이드                
+});
+ 
